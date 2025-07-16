05b19c60deff810e6dc4262ccccd747b9c91644e
Yash Patel <yashnilay@gmail.com>
2025-07-16 01:45:16 -0400

    fixed the global css for the footer.
    Fixed the flex. The page should now stretch to however large the viewport may be, no matter the amount of content exists on the page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,19 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
+
 import {Swiper, SwiperSlide} from 'swiper/react';
 
 
 import react from '@astrojs/react';
 
 
+import mdx from '@astrojs/mdx';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yashnilay.ca',
   vite: { plugins: [tailwindcss()] },
-  integrations: [react(), icon()], 
+  integrations: [react(), icon(), mdx()], 
 });
